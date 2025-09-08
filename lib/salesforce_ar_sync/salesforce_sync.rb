4c69f1eb9a4093e6953f4f479ed347d22f3d7251
Sam Gong <sgong@infotech.com>
2025-09-08 13:38:50 -0400

    add @exists_in_salesforce = true back

--- lib/salesforce_ar_sync/salesforce_sync.rb
+++ lib/salesforce_ar_sync/salesforce_sync.rb
@@ -177,6 +177,7 @@ module SalesforceArSync
     def salesforce_create_object(attributes)
       attributes[self.class.salesforce_web_id_attribute_name.to_s] = id if self.class.salesforce_sync_web_id? && !new_record?
       salesforce_id = SF_CLIENT.create!(salesforce_object_name, format_attributes(attributes))
+      @exists_in_salesforce = true
     end
 
     def salesforce_update_object(attributes)
